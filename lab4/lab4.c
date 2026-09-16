6588ea80bc1837750d83dc994182d95bd6f43268
Vitor Juliani <v255931@dac.unicamp.br>
2026-09-16 16:38:08 -0300

    adiciona lab4

---
+++ lab4/lab4.c
@@ -0,0 +1,82 @@
+#define STDIN_FD  0
+#define STDOUT_FD 1
+
+int read(int __fd, const void *__buf, int __n){
+    int ret_val;
+  __asm__ __volatile__(
+    "mv a0, %1           # file descriptor\n"
+    "mv a1, %2           # buffer \n"
+    "mv a2, %3           # size \n"
+    "li a7, 63           # syscall write code (63) \n"
+    "ecall               # invoke syscall \n"
+    "mv %0, a0           # move return value to ret_val\n"
+    : "=r"(ret_val)  // Output list
+    : "r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
+  return ret_val;
+}
+
+void write(int __fd, const void *__buf, int __n)
+{
+  __asm__ __volatile__(
+    "mv a0, %0           # file descriptor\n"
+    "mv a1, %1           # buffer \n"
+    "mv a2, %2           # size \n"
+    "li a7, 64           # syscall write (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(__fd), "r"(__buf), "r"(__n)    // Input list
+    : "a0", "a1", "a2", "a7"
+  );
+}
+
+void exit(int code)
+{
+  __asm__ __volatile__(
+    "mv a0, %0           # return code\n"
+    "li a7, 93           # syscall exit (64) \n"
+    "ecall"
+    :   // Output list
+    :"r"(code)    // Input list
+    : "a0", "a7"
+  );
+}
+
+unsigned int multiply(unsigned int a, unsigned int b)
+{
+  /* multiply logic */
+
+  return result;
+}
+
+unsigned int divide(unsigned int dividend, unsigned int divisor)
+{
+  /* divide logic */
+
+  return quotient;
+}
+
+unsigned int remainder(unsigned int dividend, unsigned int divisor)
+{
+  /* remainder logic */
+
+  return remainder;
+}
+
+int main()
+{
+  int n = read(STDIN_FD, (void*) buffer, 10);
+
+  /* program logic */
+
+  write(STDOUT_FD, (void*) buffer, n);
+
+  return 0;
+}
+
+void _start()
+{
+  int ret_code = main();
+  exit(ret_code);
+}
